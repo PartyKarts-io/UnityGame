c9f10d2c69295a387cfe844294fd7f3ef69a9497
PartyKarts.io <partykarts.io@gmail.com>
2023-05-13 00:17:29 -0700

    car selection next

--- Assets/Reach - Complete Sci-Fi UI/Scripts/Panels/ChapterIdentifier.cs
+++ Assets/Reach - Complete Sci-Fi UI/Scripts/Panels/ChapterIdentifier.cs
@@ -16,9 +16,6 @@ namespace Michsky.UI.Reach
         public ButtonManager continueButton;
         public ButtonManager playButton;
         public ButtonManager replayButton;
-        public GameObject completedIndicator;
-        public GameObject unlockedIndicator;
-        public GameObject lockedIndicator;
 
         [HideInInspector] public ChapterManager chapterManager;
         [HideInInspector] public bool isLocked;
@@ -37,17 +34,14 @@ namespace Michsky.UI.Reach
             }
         }
 
-        public void UpdateBackgroundRect() 
-        { 
+        public void UpdateBackgroundRect()
+        {
             chapterManager.currentBackgroundRect = backgroundRect;
             chapterManager.DoStretch();
         }
 
         public void SetCurrent()
         {
-            completedIndicator.SetActive(false);
-            unlockedIndicator.SetActive(true);
-            lockedIndicator.SetActive(false);
 
             continueButton.gameObject.SetActive(true);
             playButton.gameObject.SetActive(false);
@@ -61,10 +55,6 @@ namespace Michsky.UI.Reach
 
         public void SetLocked()
         {
-            completedIndicator.SetActive(false);
-            unlockedIndicator.SetActive(false);
-            lockedIndicator.SetActive(true);
-
             continueButton.gameObject.SetActive(false);
             playButton.gameObject.SetActive(true);
             replayButton.gameObject.SetActive(false);
@@ -76,10 +66,6 @@ namespace Michsky.UI.Reach
 
         public void SetUnlocked()
         {
-            completedIndicator.SetActive(false);
-            unlockedIndicator.SetActive(true);
-            lockedIndicator.SetActive(false);
-
             continueButton.gameObject.SetActive(false);
             playButton.gameObject.SetActive(true);
             replayButton.gameObject.SetActive(false);
@@ -91,10 +77,6 @@ namespace Michsky.UI.Reach
 
         public void SetCompleted()
         {
-            completedIndicator.SetActive(true);
-            unlockedIndicator.SetActive(false);
-            lockedIndicator.SetActive(false);
-
             continueButton.gameObject.SetActive(false);
             playButton.gameObject.SetActive(false);
             replayButton.gameObject.SetActive(true);
---
+++ Assets/TrackSelectManager.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using GameBalance;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackSelectManager : MonoBehaviour
+{
+    [Header("Path settings")]
+    [SerializeField] float MaxRadius = 1.5f;
+    [SerializeField] float PathStep = 10;
+
+    bool IsMultiplayer;
+    TrackPreset CurrentTrackPreset;
+    int CurrentTrackIndex = 0;
+    bool SubmitIsPressed = true;
+    bool HorizontalIsPressed = true;
+
+    const string LapCaption = "Lap";
+    const string LapsCaption = "Laps";
+
+    public Action<TrackPreset> OnSelectTrackAction { get; set; }
+
+    List<TrackPreset> Tracks { get { return WorldLoading.IsMultiplayer ? B.MultiplayerSettings.AvailableTracksForMultiplayer : B.GameSettings.Tracks; } }
+
+    void Awake()
+    {
+        SelectTrack(0);
+    }
+
+    TrackPreset GetTrackByIndex(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                return Tracks.FirstOrDefault(t => t.TrackName == "Party Park");
+            case 1:
+                return Tracks.FirstOrDefault(t => t.TrackName == "Belgian Hills (Spa)");
+            case 2:
+                return Tracks.FirstOrDefault(t => t.TrackName == "Temple of Speed");
+            default:
+                return Tracks.FirstOrDefault(t => t.TrackName == "Party Park");
+        }
+    }
+
+    public void NextTrack()
+    {
+        if (CurrentTrackIndex < 2)
+        {
+            SelectTrack(CurrentTrackIndex + 1);
+        }
+    }
+
+    public void PreviousTrack()
+    {
+        if (CurrentTrackIndex > 0)
+        {
+            SelectTrack(CurrentTrackIndex - 1);
+        }
+    }
+
+    public void SelectTrack(int trackIndex)
+    {
+        CurrentTrackIndex = trackIndex;
+        TrackPreset track = GetTrackByIndex(trackIndex);
+
+        Debug.Log(track.TrackName);
+
+        CurrentTrackPreset = track;
+        WorldLoading.LoadingTrack = CurrentTrackPreset;
+    }
+}
