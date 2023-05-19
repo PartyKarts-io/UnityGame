7353a120f4bb195a4f7543e2e819ba74442083e0
PartyKarts.io <partykarts.io@gmail.com>
2023-05-18 22:42:40 -0700

    ready for analytics

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -30,7 +30,6 @@ public class MainMenuManager : MonoBehaviour
         ThirdwebManager.Instance.nftsLoadedEvent.AddListener(NFTsLoaded);
 
         if (!Utils.IsWebGLBuild()) return;
-
     }
 
     private void ToggleAllButtons(bool shouldEnable)
